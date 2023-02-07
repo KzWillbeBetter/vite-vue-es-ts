8e7633c40fce04f9223b6b92e4402cdca1baf4ad
kz <2667422922@qq.com>
2023-02-07 17:09:03 +0800

    。

---
+++ 01语法学习/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <!--    <List></List>-->
+    <div class="border">
+      <h3>JS 父子之间通信</h3>
+      <div>MyName:{{ name }}</div>
+      <!--      <div>MyName:{{ aaa }}</div>-->
+      <HelloWorld @click="accept" :msg="obj"/>
+    </div>
+    <div class="border">
+      <h3>TS 父子之间通信</h3>
+      <Modal @click1="recevied" :msg="msg"></Modal>
+    </div>
+  </div>
+
+</template>
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal/Index.vue'
+import {provide, reactive, ref} from "vue";
+
+provide('test', '23')
+const obj = reactive({
+  info: '我是父组件的值'
+})
+const name: any = ref('god')
+const msg = ref('ZHONGGUONB')
+// const props: any = ref('kzkkz')
+// const aaa='1'
+const accept = (e: string) => {
+  console.log('父组件拿到的值', e)
+  name.value = e
+}
+
+const recevied = (e: string) => {
+  console.log('父组件拿到的值', e)
+  msg.value = 'nb'
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.border {
+  border: 1px seagreen solid;
+}
+</style>
---
+++ 01语法学习/components/List/List.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <select name="abc" v-model="name">
+      <option value="富贵老师">富贵老师</option>
+      <option value="西门老师">西门老师</option>
+      <option value="嘤嘤老师">嘤嘤老师</option>
+    </select>
+  </div>
+  <input v-model="name" type="text"/>
+  <ul v-for="item in newList" :key="item">
+    <li>{{ item.name }}</li>
+  </ul>
+</template>
+
+<script setup>
+import queryList1 from "./queryList.js"
+import addList1 from "./addList.js"
+
+const {list, name} = queryList1()
+console.log(list, name.value)
+const newList = addList1(list)
+</script>
+
+<style scoped>
+ul, li {
+  list-style: none;
+}
+</style>
