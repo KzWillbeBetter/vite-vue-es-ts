fce661d5c1d051c36a96d42f205a22666790a2b8
kz <2667422922@qq.com>
2023-02-13 09:51:16 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,31 @@
 // vite.config.ts
-import {defineConfig} from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 // @ts-ignore
-import path from 'path'
+import path from "path";
 import eslintPlugin from "vite-plugin-eslint";
 
-
 export default defineConfig({
-    plugins: [vue(),
-        eslintPlugin({
-            include: ['src/**/*.js', 'src/**/*.vue', 'src/*.js', 'src/*.vue']
-        }),
-        AutoImport({
-            resolvers: [ElementPlusResolver()],
-        }),
-        Components({
-            resolvers: [ElementPlusResolver()],
-        }),],
-    resolve: {
-        alias: {
-            "@": path.resolve("./src") // 相对路径别名配置，使用 @ 代替 src
-        }
+  plugins: [
+    vue(),
+
+    eslintPlugin({
+      include: ["src/**/*.js", "src/**/*.vue", "src/*.js", "src/*.vue"],
+    }),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve("./src"), // 相对路径别名配置，使用 @ 代替 src
     },
-
-})
-
+  },
+});
